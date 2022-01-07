a30a2bf27d2ce6e546be1c8115484766319e95f7
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2022-01-06 18:38:48 -0800

    Update UI logic to clear input and result text on each submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,25 @@
 function isLeapYear(year) {
-  if (typeof year !== "number" || year !== parseInt(year, 10)) return null;
+  if (!year || year != parseInt(year, 10)) return null;
   return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 }
 
 $(document).ready(function () {
   $(".form-group").submit(function (event) {
     event.preventDefault();
-    const year = $("#year").val();
-    if (isLeapYear(year)) {
-      $("#result").text("Leap year!");
+    $("#result").empty();
+    $("#input-error").empty();
+    const currentYear = new Date().getFullYear();
+    const inputYear = $("#year").val();
+    const result = isLeapYear(inputYear);
+    if (result) {
+      $("#result").text(inputYear + " is a leap year!");
+    } else if (result === null) {
+      $("#input-error").text(
+        "Enter a year in a format like " + currentYear + "."
+      );
     } else {
-      $("#result").text("Regular year.");
+      $("#result").text(inputYear + " is a regular year.");
     }
+    $("#year").val("");
   });
 });
