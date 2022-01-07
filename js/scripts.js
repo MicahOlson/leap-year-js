bd2c0d2398c8851eec6ef7cbcfd1997e2fece4fd
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2022-01-06 18:46:17 -0800

    Leave the input value on submit and remove year from result text

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,21 @@ function isLeapYear(year) {
 $(document).ready(function () {
   $(".form-group").submit(function (event) {
     event.preventDefault();
+    
     $("#result").empty();
     $("#input-error").empty();
+    
     const currentYear = new Date().getFullYear();
     const inputYear = $("#year").val();
     const result = isLeapYear(inputYear);
     if (result) {
-      $("#result").text(inputYear + " is a leap year!");
+      $("#result").text("Leap year!");
     } else if (result === null) {
       $("#input-error").text(
         "Enter a year in a format like " + currentYear + "."
       );
     } else {
-      $("#result").text(inputYear + " is a regular year.");
+      $("#result").text("Regular year.");
     }
-    $("#year").val("");
   });
 });
